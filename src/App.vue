c4ca5771ce5fab86b14666f99d131fa6f9961691
lyj0158 <1162168108@qq.com>
2022-03-04 02:00:50 +0800

    配置boostrap

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <my-banner></my-banner>
 </template>
+
+<script>
+import MyBanner from "@/views/MyBanner";
+export default {
+  components: {
+    MyBanner,
+  }
+}
+</script>
 
 <style>
 
